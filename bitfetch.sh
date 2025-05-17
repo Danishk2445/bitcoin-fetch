99ad43a25d0c5eba5531843b3da0c669df70e7bf
Danishk2445 <danishkhateeb03@gmail.com>
2025-05-17 10:38:33 -0500

    commit

---
+++ bitfetch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+logo=$'\e[0;33m
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⣷⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀
+⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣤⣄⠀⠀⠀⠈⠉⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢠⣶⣶⣤⡀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡆
+⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠼⣿⣿⡿⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢀⣀⣀⠀⠀⠀⠀⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⢿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠀⠀⣼⣿⣿⣿⣦⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡿
+⠸⣿⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠛⠛⠿⠟⠋⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣤⡄⠀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⡟⠀
+⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀
+⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀
+\e[0m'
+
+api_url="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&price_change_percentage=24h"
+
+bitcoin_data=$(curl -s "$api_url")
+
+if [ -n "$bitcoin_data" ]; then
+    price_24h_percentage=$(echo "$bitcoin_data" | jq -r '.[0].price_change_percentage_24h')
+    price_24h_value=$(echo "$bitcoin_data" | jq -r '.[0].price_change_24h')
+    current_price=$(echo "$bitcoin_data" | jq -r '.[0].current_price' | numfmt --grouping)
+
+    echo "$logo"
+    echo ""
+    echo -e "Current Bitcoin Price (USD): \e[0;32m\$${current_price}\e[0m"
+    echo ""
+
+    if [ -n "$price_24h_value" ] && [ -n "$price_24h_percentage" ]; then
+        price_24h_value=$(printf "%.2f" "$price_24h_value")
+        price_24h_percentage=$(printf "%.2f" "$price_24h_percentage")
+
+        sign="+"
+        color="\e[0;32m"
+        if [[ $(echo "$price_24h_value" | cut -c 1) == "-" ]]; then
+            sign=""
+            color="\e[0;31m"
+        fi
+        echo -e "Price Change (24h): ${color}${sign}${price_24h_value}\$ (${price_24h_percentage}%)\e[0m"
+    else
+        echo "Error fetching 24h price change data."
+    fi
+
+else
+    echo "Error fetching Bitcoin data."
+fi
+
+echo ""
+
